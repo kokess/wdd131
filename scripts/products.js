401116516a834aed91dcab65e8ae95711115e3c7
kokess <aessien5@byupathway.edu>
2026-02-05 14:45:59 +0100

    commit

--- scripts/products.js
+++ scripts/products.js
@@ -1,16 +1,45 @@
 const products = [
-    { id: "fc-1888", name: "Flux Capacitor" },
-    { id: "fs-1987", name: "Flame Sensor" },
-    { id: "ac-2000", name: "Air Conditioner" },
-    { id: "sp-2020", name: "Solar Panel" },
-    { id: "wb-2023", name: "Water Boiler" }
+    {
+        id: "fc-1888",
+        name: "Flux Capacitor"
+    },
+    {
+        id: "fs-1987",
+        name: "Flame Sensor"
+    },
+    {
+        id: "ac-2000",
+        name: "Air Conditioner"
+    },
+    {
+        id: "sp-2020",
+        name: "Solar Panel"
+    },
+    {
+        id: "wb-2023",
+        name: "Water Boiler"
+    },
+    {
+        id: "rf-2024",
+        name: "Refrigerator"
+    }
 ];
 
-const productSelect = document.querySelector("#product");
+document.addEventListener("DOMContentLoaded", function () {
 
-products.forEach(product => {
-    const option = document.createElement("option");
-    option.value = product.id;
-    option.textContent = product.name;
-    productSelect.appendChild(option);
+    const productSelect = document.getElementById("product");
+
+    products.forEach(function (product) {
+
+        const option = document.createElement("option");
+
+        option.value = product.id;
+
+        option.textContent = product.name;
+
+        
+        productSelect.appendChild(option);
+
+    });
+
 });
